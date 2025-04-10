3664684c44bd35d79157bb18a7a395ce94096ac1
GregulasM <miha125rus@yandex.ru>
2025-04-11 05:29:45 +1000

    Сделал иерархию папок для будущего слияния всех веток паттернов

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Только начало");
+        List<string> папки = new List<string>();
+
+        string папки_для_добавления =
+            "dp_abstract_factory dp_adapter dp_bridge dp_chain dp_composite dp_decorator dp_factory dp_observer dp_proxy dp_singleton dp_strategy";
+        
+        string[] обработанные_папки = папки_для_добавления.Split(" ");
+
+        foreach (var папка in обработанные_папки)
+        {
+            Directory.CreateDirectory($"D:\\Vasnie Progi\\C# projects\\DesignPatterns_Csharp\\{папка}");
+        }
     }
 }
