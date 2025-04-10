6e75b273c66b49e3a0a0dd8c67448315e3dddc71
GregulasM <miha125rus@yandex.ru>
2025-04-11 04:09:08 +1000

    Загрузил Factory паттерн недельной давности))

---
+++ ITransport_common.cs
@@ -0,0 +1,9 @@
+namespace FactoryClass;
+
+public interface ITransport_common
+{
+    public void Водитель_тип_транспорт();
+    public void Проверка_топлива_транспорт();
+    public void Починить_транспорт();
+    public void Показать_характеристики_транспорт();
+}
